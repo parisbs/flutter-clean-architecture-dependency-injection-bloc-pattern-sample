e7e7ac5ac18b9286b172d84ba2566f9f652656dd
Paris N. Baltazar Salguero <32491290+parisbs@users.noreply.github.com>
2022-07-29 16:53:21 -0500

    Add screen readers support for web

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:apitest/injection_container.dart' as di;
 import 'package:apitest/presentation/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Required to enable screen readers support for web
+  RendererBinding.instance.setSemanticsEnabled(true);
   await di.init();
   runApp(const App());
 }
